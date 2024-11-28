d3717738543fffca82565f571e415a1b26a5bf37
hichamoubaha <oubahahicham7@gmail.com>
2024-11-28 11:49:05 +0100

    add the function of player select

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ let players = [];
             players = data.players;
             populatePlayerSelect();
         }
+        function populatePlayerSelect() {
+            const playerSelect = document.getElementById('playerSelect');
+            players.forEach(player => {
+                const option = document.createElement('option');
+                option.value = player.position.toLowerCase();
+                option.text = player.name;
+                playerSelect.add(option);
+            });
+        }
+
+        function openModal(position) {
+            currentPosition = position;
+            const modal = document.getElementById('playerModal');
+            modal.style.display = 'block';
+        }
+
+        function closeModal() {
+            const modal = document.getElementById('playerModal');
+            modal.style.display = 'none';
+        }
